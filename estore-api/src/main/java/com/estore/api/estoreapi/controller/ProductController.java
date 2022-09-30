8305bb15dcde13873a69d18f52496444f4232967
Thomas <thomasakulla@gmail.com>
2022-09-29 23:05:18 -0400

    added functionality to searchProduct via product name

--- estore-api/src/main/java/com/estore/api/estoreapi/controller/ProductController.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/controller/ProductController.java
@@ -13,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.estore.api.estoreapi.Model.Product;
@@ -49,5 +50,22 @@ public class ProductController {
         }
     }
 
+    @GetMapping("product/")
+    public ResponseEntity<Product[]> searchProduct(@RequestParam String name) {
+        LOG.info("GET /admin/product/?Name="+name);
+
+        try {
+            Product[] products = productDao.findProduct(name);
+            if (products.length > 0){
+                return new ResponseEntity<Product[]>(products, HttpStatus.OK);
+            }
+            else{
+                 return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+        } catch (IOException e) {
+            LOG.log(Level.SEVERE,e.getLocalizedMessage());
+            return new ResponseEntity<Product[]>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 
 }
