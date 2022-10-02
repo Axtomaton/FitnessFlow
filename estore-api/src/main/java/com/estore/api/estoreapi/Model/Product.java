84fd4299cb3da43140e251b28662934993b6969d
Thomas <thomasakulla@gmail.com>
2022-10-02 12:28:41 -0400

    added javadocs and edited a few minor things

--- estore-api/src/main/java/com/estore/api/estoreapi/Model/Product.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/Model/Product.java
@@ -4,6 +4,11 @@ import java.util.logging.Logger;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+/**
+ * Represents a Product entity
+ * 
+ * @author aagmanrelan
+ */
 public class Product {
     
     private static final Logger LOG = Logger.getLogger(Product.class.getName());
@@ -19,13 +24,25 @@ public class Product {
     private String Room_Number;
     private boolean Available;
 
-
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public String toString() {
         String Product="Product(ID:"+id+", Name:"+Name+", Type:"+Type+", Instructor:"+Instructor+", Room:"+Room_Number+", Available:"+Available+")";
         return Product;
     }
 
+    /**
+     * Create a product with the given id and name
+     * @param id The id of the product
+     * @param name The name of the product
+     * 
+     * {@literal @}JsonProperty is used in serialization and deserialization
+     * of the JSON object to the Java object in mapping the fields.  If a field
+     * is not provided in the JSON object, the Java field gets the default Java
+     * value, i.e. 0 for int
+     */
     public Product(@JsonProperty("id") int id, @JsonProperty("Name") String Name, @JsonProperty("Type") String Type, @JsonProperty("Instructor") String Instructor, 
                         @JsonProperty("Room_Number") String Room_Number, @JsonProperty("Available") boolean Available) {
         this.id = id;
@@ -36,47 +53,96 @@ public class Product {
         this.Available = Available;
     }
 
+    /**
+     * Retrieves the id of the product
+     * @return The id of the product
+     */
     @JsonProperty("id") 
     public int getID(){
         return id;
     }
+
+    /**
+     * Retrieves the type of the product
+     * @return The type of the product
+     */
     @JsonProperty("Type") 
     public String getType(){
         return Type;
     }
+
+    /**
+     * Retrieves the name of the product
+     * @return The name of the product
+     */
     @JsonProperty("Name") 
     public String getName(){
         return Name;
     }
+
+    /**
+     * Retrieves the instructor of the product
+     * @return The instructor of the product
+     */
     @JsonProperty("Instructor")
     public String getInstructor(){
         return Instructor;
     }
+
+    /**
+     * Retrieves the room number of the product
+     * @return The room number of the product
+     */
     @JsonProperty("Room_Number") 
     public String get_Room_Number(){
         return Room_Number;
     }
+
+    /**
+     * Retrieves the availibility (boolean) of the product
+     * @return The boolean of the product
+     */
     @JsonProperty("Available") 
     public boolean IsAvailable(){
         return Available;
     }
 
-
+    /**
+     * Sets the name of the product - necessary for JSON object to Java object deserialization
+     * @param name The name of the product
+     */
     public void setName(String Name){
         this.Name = Name;
     }
 
+    /**
+     * Sets the instructor of the product - necessary for JSON object to Java object deserialization
+     * @param Instructor The name of the product
+     */
     public void setInstructor(String Instructor){
         this.Instructor=Instructor;
     }
 
+    /**
+     * Sets the type of the product - necessary for JSON object to Java object deserialization
+     * @param Type The name of the product
+     */
     public void setType(String Type){
         this.Type=Type;
     }
 
+    /**
+     * Sets the room number of the product - necessary for JSON object to Java object deserialization
+     * @param Room_Number The name of the product
+     */
     public void set_Room_Number(String Room_Number){
         this.Room_Number=Room_Number;
     }
+
+    /**
+     * Sets the availability of the product - necessary for JSON object to Java object deserialization
+     * @param Available The name of the product
+     */
     public void setAvailability(boolean Available){
         this.Available=Available;
     }
--- estore-api/src/main/java/com/estore/api/estoreapi/persistence/ProductFileDAO.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/persistence/ProductFileDAO.java
@@ -14,52 +14,110 @@ import org.springframework.stereotype.Component;
 import com.estore.api.estoreapi.Model.Product;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+/**
+ * Implements the functionality for JSON file-based peristance for Products
+ * 
+ * {@literal @}Component Spring annotation instantiates a single instance of this
+ * class and injects the instance into other classes as needed
+ * 
+ * @author aagmanrelan
+ */
 @Component
 public class ProductFileDAO implements ProductDAO{
 
     private static final Logger LOG = Logger.getLogger(ProductFileDAO.class.getName());
 
-    Map<Integer,Product> Products;
+    Map<Integer,Product> Products;  // Provides a local cache of the product objects
+                                    // so that we don't need to read from the file
+                                    // each time
 
-    private ObjectMapper objectMapper;  
+    private ObjectMapper objectMapper;  // Provides conversion between Product
+                                        // objects and JSON text format written
+                                        // to the file
 
-    private static int nextId;  
-    private String filename;    
+    private static int nextId;  // The next Id to assign to a new product
+    private String filename;    // Filename to read from and write to
 
+    /**
+     * Creates a Product File Data Access Object
+     * 
+     * @param filename Filename to read from and write to
+     * @param objectMapper Provides JSON Object to/from Java Object serialization and deserialization
+     * 
+     * @throws IOException when file cannot be accessed or read from
+     */
     @Autowired
     public ProductFileDAO(@Value("${products.file}")String filename,ObjectMapper objectmapper) throws IOException{
             this.filename=filename;
             this.objectMapper = objectmapper;
-            load();
+            load(); // load the products from the file
     }
 
+    /**
+     * Loads {@linkplain Product products} from the JSON file into the map
+     * <br>
+     * Also sets next id to one more than the greatest id found in the file
+     * 
+     * @return true if the file was read successfully
+     * 
+     * @throws IOException when file cannot be accessed or read from
+     */
     private boolean load() throws IOException {
         Products = new TreeMap<>();
         nextId = 0;
 
+        // Deserializes the JSON objects from the file into an array of products
+        // readValue will throw an IOException if there's an issue with the file
+        // or reading from the file
         Product[] productArray = objectMapper.readValue(new File(filename),Product[].class);
+
+        // Add each product to the tree map and keep track of the greatest id
         for (Product product : productArray) {
             Products.put(product.getID(),product);
             if (product.getID() > nextId)
                 nextId = product.getID();
         }
+
+        // Make the next id one greater than the maximum from the file
         ++nextId;
         return true;
     }
 
+    /**
+     * Saves the {@linkplain Product products} from the map into the file as an array of JSON objects
+     * 
+     * @return true if the {@link Product products} were written successfully
+     * 
+     * @throws IOException when file cannot be accessed or written to
+     */
     private boolean save() throws IOException {
         Product[] ProductArray = getProductsArray();
 
-
+        // Serializes the Java Objects to JSON objects into the file
+        // writeValue will thrown an IOException if there is an issue
+        // with the file or reading from the file
         objectMapper.writeValue(new File(filename),ProductArray);
         return true;
     }
     
-
+    /**
+     * Generates an array of {@linkplain Product products} from the tree map
+     * 
+     * @return  The array of {@link Product products}, may be empty
+     */
     private Product[] getProductsArray(){
         return getProductsArray(null);
     }
 
+    /**
+     * Generates an array of {@linkplain Product products} from the tree map for any
+     * {@linkplain Product products} that contains the text specified by containsText
+     * <br>
+     * If containsText is null, the array contains all of the {@linkplain Product products}
+     * in the tree map
+     * 
+     * @return  The array of {@link Product products}, may be empty
+     */
     private Product[] getProductsArray(String Name){
         ArrayList<Product> productarraylist = new ArrayList<>();
         for (Product product: Products.values()){
@@ -72,14 +130,20 @@ public class ProductFileDAO implements ProductDAO{
         return productArray;
     }
 
-
+    /**
+     * Generates the next id for a new {@linkplain Product product}
+     * 
+     * @return The next id
+     */
     private synchronized static int nextId() {
         int id = nextId;
         ++nextId;
         return id;
     }
 
-    
+    /**
+    ** {@inheritDoc}
+     */
     @Override
     public Product[] getProducts() throws IOException {
             synchronized(Products){
@@ -87,6 +151,9 @@ public class ProductFileDAO implements ProductDAO{
             }
     }
 
+    /**
+    ** {@inheritDoc}
+     */
     @Override
     public Product[] findProduct(String name) throws IOException {
         synchronized(Products){
@@ -94,6 +161,9 @@ public class ProductFileDAO implements ProductDAO{
         }
     }
 
+    /**
+    ** {@inheritDoc}
+     */
     @Override
     public Product getProduct(int id) throws IOException {
         synchronized(Products){
@@ -106,17 +176,22 @@ public class ProductFileDAO implements ProductDAO{
         }
     }
 
-
+    /**
+    ** {@inheritDoc}
+     */
     @Override
     public Product createProduct(Product product) throws IOException {
         synchronized(Products){
-            Product newProduct = new Product(nextId, product.getName(), product.getType(), product.getInstructor(), product.get_Room_Number(), product.IsAvailable());
+            Product newProduct = new Product(nextId(), product.getName(), product.getType(), product.getInstructor(), product.get_Room_Number(), product.IsAvailable());
             Products.put(newProduct.getID(),newProduct);
             save();
             return newProduct;
         }
     }
 
+    /**
+    ** {@inheritDoc}
+     */
     @Override
     public Product updateProduct(Product product) throws IOException {
         synchronized(Products){
@@ -131,6 +206,9 @@ public class ProductFileDAO implements ProductDAO{
         }
     }
 
+    /**
+    ** {@inheritDoc}
+     */
     @Override
     public boolean deleteProduct(int id) throws IOException {
         synchronized(Products){
