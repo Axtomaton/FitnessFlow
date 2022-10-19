d89209cce924e8ca75d8815ab228242e7f37bcc1
Ivan Lin <il9082@rit.edu>
2022-10-19 19:26:34 -0400

    updated with the logo

--- estore-ui/estore-ui/src/app/app-routing-module.ts
+++ estore-ui/estore-ui/src/app/app-routing-module.ts
@@ -3,13 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { ShoppingcartComponent } from './shoppingcart/shoppingcart.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
-import { ContactPageComponent } from './contact-page/contact-page.component';
 import { AboutPageComponent } from './about-page/about-page.component';
 
 export const routes: Routes = [
   {path: 'shoppingcart', component: ShoppingcartComponent},
   {path: 'login', component:LoginComponent},
-  {path: 'contact', component:ContactPageComponent},
   {path: 'about', component:AboutPageComponent},
   {path: 'signup',component:SignupComponent}
 ];
@@ -19,4 +17,4 @@ export const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-// export const routingComponents = [ShoppingcartComponent, LoginComponent, SignupComponent,ContactPageComponent,AboutPageComponent]
+export const routingComponents = [ShoppingcartComponent, LoginComponent, SignupComponent,AboutPageComponent]
