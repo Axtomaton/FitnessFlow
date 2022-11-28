7a25321bfa42acda07dfc4bd383d652c9cfcd634
Thomas <thomasakulla@gmail.com>
2022-11-28 18:01:53 -0500

    ivan update this

---
+++ estore-ui/estore-ui/src/app/customer-view/script-1.js
@@ -0,0 +1,6 @@
+function changeDisplay(){
+    
+    document.getElementById('melisssa-instructor').style.display = 'block';
+    document.getElementById('instructor-name-melissa').style.display = 'block';
+    document.getElementById('class-type-abs').style.display = 'block';
+}
