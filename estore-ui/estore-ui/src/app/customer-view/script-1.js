e3880c7eb6105c4cc27230d2005f0d83dc20fa0d
Thomas <thomasakulla@gmail.com>
2022-11-29 10:45:52 -0500

    working on customer-view component

--- estore-ui/estore-ui/src/app/customer-view/script-1.js
+++
@@ -1,6 +0,0 @@
-function changeDisplay(){
-    
-    document.getElementById('melisssa-instructor').style.display = 'block';
-    document.getElementById('instructor-name-melissa').style.display = 'block';
-    document.getElementById('class-type-abs').style.display = 'block';
-}
